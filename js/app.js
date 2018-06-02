042f9f5118ca6ac4f96c8ca1eddc088f6110fd41
Sara Blevins <dvlnthebludrs@gmail.com>
2018-06-02 18:38:44 -0400

    Add functions from FEND tutorial

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 // creates cards
 const cards = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bomb', 'fa fa-bicycle', 'fa fa-bicycle'];
 //
-const bottom = [];
-const top = [];
-const turnedOver = 0;
+// const bottom = [];
+// const top = [];
+// const turnedOver = 0;
+
+// Function card HTML from FEND tutorial https://youtu.be/_rUH-sEs68Y
+function generateCard(card) {
+    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -20,20 +26,53 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// pass in card, turn into string from FEND tutorial https://youtu.be/_rUH-sEs68Y
+function initGame() {
+    var deck = document.querySelector('.deck');
+    var cardHTML = shuffle(cards).map(function (card) {
+        return generateCard(card);
+    });
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+    deck.innerHTML = cardHTML.join('');
+
+    var allCards = document.querySelectorAll('.card');
+    var openCards = [];
+
+
+    allCards.forEach(function (card) {
+        card.addEventListener('click', function (e) {
+
+            if (!card.classList.contains('open') &&
+                !card.classList.contains('show') &&
+                !card.classList.contains('match')
+            ) {
+                openCards.push(card);
+                card.classList.add('open', 'show');
+
+                if (openCards.length == 2) {
+                    moveCount();
+                    //If cards match, leave facing up
+                    if (openCards[0].dataset.card == openCards[1].dataset.card) {
+                        openCards[0].classList.add('match');
+                        openCards[0].classList.add('open');
+                        openCards[0].classList.add('show');
+
+                        openCards[1].classList.add('match');
+                        openCards[1].classList.add('open');
+                        openCards[1].classList.add('show');
+
+                        openCards = [];
+                    } else {
+                        //If cards do not match, flip cards back over
+                        setTimeout(function () {
+                            openCards.forEach(function (card) {
+                                card.classList.remove('open', 'show');
+                            });
+                            openCards = [];
+                        }, 1000);
+                    }
+                }
+            }
+        });
+    });
+}
