69d65a8a384175e410017286ac719ffa1ef4f3f6
Sara Blevins <dvlnthebludrs@gmail.com>
2018-06-02 20:03:19 -0400

    Add modal raw code from w2schools

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
-// creates cards
-const cards = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bomb', 'fa fa-bicycle', 'fa fa-bicycle'];
-//
-// const bottom = [];
-// const top = [];
-// const turnedOver = 0;
+var cards = ['fa-code-fork', 'fa-code-fork',
+    'fa-cogs', 'fa-cogs',
+    'fa-user-secret', 'fa-user-secret',
+    'fa-terminal', 'fa-terminal',
+    'fa-space-shuttle', 'fa-space-shuttle',
+    'fa-sitemap', 'fa-sitemap',
+    'fa-i-cursor', 'fa-i-cursor',
+    'fa-gamepad', 'fa-gamepad'
+];
 
-// Function card HTML from FEND tutorial https://youtu.be/_rUH-sEs68Y
+// Returns card HTML function from https://youtu.be/_rUH-sEs68Y
 function generateCard(card) {
-    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -22,7 +25,6 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
@@ -75,4 +77,51 @@ function initGame() {
             }
         });
     });
+    // Timer that resets with restart button
+    var timer = document.querySelector('.timer');
+    var timing;
+    var second = 0;
+
+    function startTimer() {
+        timing = window.setInterval(function () {
+            timer.innerHTML = second + " secs"
+            second++;
+        }, 1000);
+    }
+
+    function resetTimer() {
+        clearInterval(interval);
+    }
+    startTimer();
+
+    document.querySelector('.restart').addEventListener('click', resetTimer);
+}
+
+// build modal
+// from https: //www.w3schools.com/howto/howto_css_modals.asp
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
