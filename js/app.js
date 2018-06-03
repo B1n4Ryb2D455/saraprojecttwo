899c08b0c97d07d39cceefe7c16ca546c904cb11
Sara Blevins <dvlnthebludrs@gmail.com>
2018-06-03 10:30:55 -0400

    Add star remover with timer but not functional yet

--- js/app.js
+++ js/app.js
@@ -41,9 +41,29 @@ function initGame() {
 
     deck.innerHTML = cardHTML.join('');
 }
-
 initGame();
 
+// timer and stars remover
+    var moves = 0;
+    var moveCounter = document.querySelector('.moves');
+    var stars = document.querySelectorAll('.stars');
+
+function removeStars() {
+    moves += 1;
+    moveCounter.innerHTML = moves;
+
+    if (moves > 8 && moves <= 13) {
+        var removeOne = document.querySelector('.starOne');
+        removeOne.style.display='none';
+        }
+        else if (moves >= 14) {
+        var removeTwo = document.querySelector('.starTwo');
+        removeTwo.style.display='none';
+        }
+}
+
+
+// keep matching cards open from FEND tutorial <insert link>
     var allCards = document.querySelectorAll('.card');
     var openCards = [];
     var moves = 0;
@@ -104,40 +124,32 @@ initGame();
     document.querySelector('.restart').addEventListener('click', resetTimer);
 //}
 
-// build modal
-// from https://www.w3schools.com/howto/howto_css_modals.asp
+//build modal
+//from https://www.w3schools.com/howto/howto_css_modals.asp
 
-// Get the modal
-// var modal = document.getElementById('myModal');
+//Get the modal
+var modal = document.getElementById('myModal');
 
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// // When the user clicks on the button, open the modal
-// btn.onclick = function () {
-//     modal.style.display = "block";
-// }
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function () {
-//     modal.style.display = "none";
-// }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function (event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
-// move counter
-// const moves = 0;
-// const counter = document.querySelector('.moves');
-
-// function countMoves() {
-//     moves++;
-//     counter.innerHTML = moves;
-// }
