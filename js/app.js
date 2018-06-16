70784ae34be12df349cd6b95d0226c401f7028c7
Sara Blevins <dvlnthebludrs@gmail.com>
2018-06-15 22:49:25 -0400

    Fix star rating

--- js/app.js
+++ js/app.js
@@ -5,8 +5,7 @@ var cards = ['fa-code-fork', 'fa-code-fork',
     'fa-space-shuttle', 'fa-space-shuttle',
     'fa-sitemap', 'fa-sitemap',
     'fa-i-cursor', 'fa-i-cursor',
-    'fa-gamepad', 'fa-gamepad'
-];
+    'fa-gamepad', 'fa-gamepad'];
 
 // Returns card HTML function from https://youtu.be/_rUH-sEs68Y
 function generateCard(card) {
@@ -31,133 +30,95 @@ function shuffle(array) {
 // pass in card, turn into string from FEND tutorial https://youtu.be/_rUH-sEs68Y
 function initGame() {
     var deck = document.querySelector('.deck');
-    var moveCounter = document.querySelector('.moves');
-
     var cardHTML = shuffle(cards).map(function (card) {
         return generateCard(card);
     });
-    moves = 0;
-    moveCounter.innerText = moves;
 
     deck.innerHTML = cardHTML.join('');
-}
-initGame();
 
-// starting the game
-//window.onload = initGame();
+    var allCards = document.querySelectorAll('.card');
+    var openCards = [];
 
-// keep matching cards open from FEND tutorial <insert link>
-var allCards = document.querySelectorAll('.card');
-var openCards = [];
-var moves = 0;
 
-allCards.forEach(function (card) {
-    card.addEventListener('click', function (e) {
+    allCards.forEach(function (card) {
+        card.addEventListener('click', function (e) {
 
-        if (!card.classList.contains('open') && !card.classList.contains('show')) {
-            openCards.push(card);
-            card.classList.add('open', 'show');
+            if (!card.classList.contains('open') &&
+                !card.classList.contains('show') &&
+                !card.classList.contains('match')
+            ) {
+                openCards.push(card);
+                card.classList.add('open', 'show');
 
-            if (openCards.length == 2) {
-                //If cards match, leave facing up
-                if (openCards[0].dataset.card == openCards[1].dataset.card) {
-                    openCards[0].classList.add('match');
-                    openCards[0].classList.add('open');
-                    openCards[0].classList.add('show');
+                if (openCards.length == 2) {
+                    moveCount();
+                    //If cards match, leave facing up
+                    if (openCards[0].dataset.card == openCards[1].dataset.card) {
+                        openCards[0].classList.add('match');
+                        openCards[0].classList.add('open');
+                        openCards[0].classList.add('show');
 
-                    openCards[1].classList.add('match');
-                    openCards[1].classList.add('open');
-                    openCards[1].classList.add('show');
-
-                    openCards = [];
-                } else {
-                    //If cards do not match, flip cards back over
-                    setTimeout(function () {
-                        openCards.forEach(function (card) {
-                            card.classList.remove('open', 'show');
-                        });
+                        openCards[1].classList.add('match');
+                        openCards[1].classList.add('open');
+                        openCards[1].classList.add('show');
 
                         openCards = [];
-                    }, 1000);
+                    } else {
+                        //If cards do not match, flip cards back over
+                        setTimeout(function () {
+                            openCards.forEach(function (card) {
+                                card.classList.remove('open', 'show');
+                            });
+                            openCards = [];
+                        }, 1000);
+                    }
                 }
-
-                moves += 1;
-                moveCounter.innerText = moves;
             }
-        }
+        });
     });
-});
-// Timer that resets with restart button
-var timer = document.querySelector('.timer');
-var timing;
-var second = 0;
 
-function startTimer() {
-    timing = window.setInterval(function () {
-        timer.innerHTML = second + " secs"
-        second++;
-    }, 1000);
+    // Timer that resets with restart button
+    var timer = document.querySelector('.timer');
+    var timing;
+    var second = 0;
+
+    function startTimer() {
+        timing = window.setInterval(function () {
+            timer.innerHTML = second + " secs"
+            second++;
+        }, 1000);
+    }
+
+    function resetTimer() {
+        clearInterval(interval);
+    }
+    startTimer();
+
+    document.querySelector('.restart').addEventListener('click', resetTimer);
 }
 
-function resetTimer() {
-    clearInterval(interval);
-}
-startTimer();
-
-document.querySelector('.restart').addEventListener('click', resetTimer);
-//}
-
-// move counter and rating
-var moveCounter = document.querySelector('.moves');
+// Move counter
 var moves = 0;
+var moveCounter = document.querySelector('.moves');
+var starOne = document.querySelector('#sO');
+var starTwo = document.querySelector('#sT');
 //var stars = document.querySelector('.stars');
-moveCounter.innerHTML = 0;
 
-function addMove() {
+function moveCount() {
     moves++;
     moveCounter.innerHTML = moves;
-
-    if (moves > 1 && moves < 12) {
-        var parent = document.getElementById("myList");
-        var child = document.getElementById("sO");
-        parent.removeChild(child);
+    //Begin removing stars based on move count
+    if (moves > 8 && moves <= 12) {
+        starOne.style.display = 'none';
+        //stars.removeChild(stars.childNodes[1]);
     } else if (moves > 13) {
-        var parent = document.getElementById("myList");
-        var child = document.getElementById("sT");
-        parent.removeChild(child);
+        starTwo.style.display = 'none';
+        //stars.removeChild(stars.childNodes[2]);
     }
+
 }
-//build modal
-//from https://www.w3schools.com/howto/howto_css_modals.asp
-var modal = document.querySelector('.modal');
-var win = document.querySelectorAll('.card', '.open', '.show', '.match');
-//game over open modal
-function gameOver() {
 
-    if (win.length == 1) {
-        //// clearInterval(timing);
-        //finalTime = timer.innerHTML;
-        modal.style.display = "block";
-        //modal.classList.add("show");
-    }
-    // else if (win.length x >= 0 && x <= 15) {
-    //     modal.style.display = "none";
-    // }
-};
-//gameOver();
+initGame();
 
 
-// Get the <span> element that closes the modal
-//var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-//span.onclick = function () {
-//modal.style.display = "none";
-//}
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function (event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
