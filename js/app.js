f328ce0d0b289c22c0dd5de9848c4d2656176e1f
Sara Blevins <dvlnthebludrs@gmail.com>
2018-06-17 14:48:21 -0400

    Remove modal buttom ask to play again

--- js/app.js
+++ js/app.js
@@ -111,7 +111,6 @@ function initGame() {
     clearInterval(interval);
   }
   startTimer();
-
   document.querySelector(".restart").addEventListener("click", resetTimer);
 }
 
@@ -135,39 +134,23 @@ function moveCount() {
     starCount = 1;
     console.log(starCount);
   }
-var youWin = document.querySelector('.content');
-youWin.innerHTML = `Congrats! Your score is ${starCount} and it took you ${moves} moves`;
+  var youWin = document.querySelector('.content');
+  youWin.innerHTML = `Congrats, you da bomb diggity! Your score is ${starCount} and it took you ${moves} moves to finish... wanna play again?`;
 }
 
 // let match = [...document.querySelectorAll('.match')];
-
 initGame();
-
 /// Get the modal
 var modal = document.getElementById('myModal');
 // var youWin = document.querySelector('.content');
 // youWin.innerHTML = `Congrats! Your score is ${starCount} and it took you ${moves} moves`;
-
 //modal.style.display = 'block';
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
+//var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
